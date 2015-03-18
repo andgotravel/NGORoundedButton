ead070c8dede334c58495a5fc492a4609bf0116a
Stas Zhukovskiy <stzhuk@gmail.com>
2015-03-18 17:39:23 +0300

    NGORounded button component initial commit

--- NGORoundedButton.podspec
+++ NGORoundedButton.podspec
@@ -1,38 +1,14 @@
-#
-# Be sure to run `pod lib lint NGORoundedButton.podspec' to ensure this is a
-# valid spec and remove all comments before submitting the spec.
-#
-# Any lines starting with a # are optional, but encouraged
-#
-# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
-#
-
 Pod::Spec.new do |s|
   s.name             = "NGORoundedButton"
-  s.version          = "0.1.0"
-  s.summary          = "A short description of NGORoundedButton."
-  s.description      = <<-DESC
-                       An optional longer description of NGORoundedButton
-
-                       * Markdown format.
-                       * Don't worry about the indent, we strip it!
-                       DESC
-  s.homepage         = "https://github.com/<GITHUB_USERNAME>/NGORoundedButton"
-  # s.screenshots     = "www.example.com/screenshots_1", "www.example.com/screenshots_2"
+  s.version          = "1.0.0"
+  s.description      = "A circle button for common actions. Icons are drawn in CoreGraphics."
+  s.homepage         = "https://github.com/andgotravel/NGORoundedButton"
   s.license          = 'MIT'
   s.author           = { "Stas Zhukovskiy" => "stzhuk@gmail.com" }
-  s.source           = { :git => "https://github.com/<GITHUB_USERNAME>/NGORoundedButton.git", :tag => s.version.to_s }
-  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
-
-  s.platform     = :ios, '7.0'
-  s.requires_arc = true
-
-  s.source_files = 'Pod/Classes/**/*'
-  s.resource_bundles = {
-    'NGORoundedButton' => ['Pod/Assets/*.png']
-  }
-
-  # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.platform         = :ios, '8.0'
+  s.requires_arc     = true
+  s.source_files     = 'Pod/Classes/**/*'
+  s.frameworks       = 'UIKit', 'CoreGraphics'
+  s.source           = { :git => "https://github.com/andgotravel/NGORoundedButton.git",
+                         :tag => "v#{s.version.to_s}" }
 end
---
+++ Pod/Classes/NGORoundedButton.m
@@ -0,0 +1,201 @@
+//
+//  NGORoundedButton.m
+//
+//
+//  Created by Stanislav Zhukovskiy on 18.03.15.
+//  Copyright (c) 2015 Ampersand. All rights reserved.
+//
+
+#import "NGORoundedButton.h"
+
+@implementation NGORoundedButton
+
+#pragma mark - Init
+
+- (instancetype)init {
+    
+    if (self = [super init]) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    
+    if (self = [super initWithFrame:frame]) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithSize:(CGFloat)size {
+    
+    self = [self initWithFrame:CGRectMake(0, 0, size, size)];
+    return self;
+}
+
+- (instancetype)initWithDefaultSize {
+    
+    CGFloat defaultSize = 44.f;
+    self = [self initWithFrame:CGRectMake(0, 0, defaultSize, defaultSize)];
+    return self;
+}
+
+- (instancetype)initWithButtonType:(NGORoundedButtonType)type {
+    
+    self.type   = type;
+    self        = [self initWithDefaultSize];
+    return self;
+}
+
+- (void)setup {
+    
+    [self setupCornerRadius];
+    [self setupShadow];
+
+    switch (self.type) {
+        case NGORoundedButtonTypeSave:      [self setupSaveButton];     break;
+        case NGORoundedButtonTypeCancel:    [self setupCancelButton];   break;
+        case NGORoundedButtonTypeBack:      [self setupBackButton];     break;
+        default: break;
+    }
+    
+    [self setupTargets];
+}
+
+#pragma mark - Buttons
+
+- (void)setupSaveButton {
+    
+    UIColor *backColor      = [UIColor colorWithRed:34/255.0 green:226/255.0 blue:157/255.0 alpha:1];
+    self.backgroundColor    = backColor;
+    
+    CAShapeLayer *checkmarkShape    = [CAShapeLayer layer];
+    checkmarkShape.lineWidth        = 3 / [UIScreen mainScreen].scale;
+    checkmarkShape.masksToBounds    = NO;
+    checkmarkShape.lineCap          = kCALineCapRound;
+    checkmarkShape.strokeColor      = [UIColor whiteColor].CGColor;
+    checkmarkShape.fillColor        = backColor.CGColor;
+    
+    CGRect fr       = self.bounds;
+    CGFloat width   = CGRectGetWidth(fr);
+    CGFloat height  = CGRectGetHeight(fr);
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, CGRectGetMidX(fr) - (width / 4), CGRectGetMidY(fr) + (height / 16));
+    CGPathAddLineToPoint(path, NULL, CGRectGetMidX(fr) - (width / 12), CGRectGetMidY(fr) + (height / 5));
+    CGPathAddLineToPoint(path, NULL, CGRectGetMidX(fr) + (width / 5), CGRectGetMidY(fr) - (height / 8));
+    
+    checkmarkShape.path = path;
+    CGPathRelease(path);
+    
+    [self.layer addSublayer:checkmarkShape];
+}
+
+- (void)setupCancelButton {
+    
+    UIColor *backgroundColor    = [UIColor whiteColor];
+    UIColor *borderColor        = [UIColor colorWithRed:227/255.0 green:231/255.0 blue:236/255.0 alpha:0.9];
+    UIColor *strokeColor        = [UIColor colorWithRed:73/255.0 green:78/255.0 blue:91/255.0 alpha:1.0];
+    
+    self.backgroundColor                = backgroundColor;
+    self.layer.cornerRadius             = CGRectGetWidth(self.bounds) / 2;
+    self.layer.borderWidth              = 1 / [UIScreen mainScreen].scale;
+    self.layer.borderColor              = borderColor.CGColor;
+    self.layer.allowsEdgeAntialiasing   = YES;
+    
+    CAShapeLayer *crossShape    = [CAShapeLayer layer];
+    crossShape.lineWidth        = 3 / [UIScreen mainScreen].scale;
+    crossShape.masksToBounds    = NO;
+    crossShape.lineCap          = kCALineCapRound;
+    crossShape.strokeColor      = strokeColor.CGColor;
+    
+    CGRect fr       = self.bounds;
+    CGFloat ofSize  = CGRectGetWidth(fr) / 2.8;
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, ofSize, ofSize);
+    CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(fr) - ofSize, CGRectGetMaxY(fr) - ofSize);
+    CGPathMoveToPoint(path, NULL, CGRectGetMaxX(fr) - ofSize, CGRectGetMinY(fr) + ofSize);
+    CGPathAddLineToPoint(path, NULL, CGRectGetMinX(fr) + ofSize, CGRectGetMaxY(fr) - ofSize);
+    
+    crossShape.path = path;
+    CGPathRelease(path);
+    
+    [self.layer addSublayer:crossShape];
+}
+
+- (void)setupBackButton {
+    
+    UIColor *backgroundColor    = [UIColor whiteColor];
+    UIColor *borderColor        = [UIColor colorWithRed:227/255.0 green:231/255.0 blue:236/255.0 alpha:0.9];
+    UIColor *strokeColor        = [UIColor colorWithRed:73/255.0 green:78/255.0 blue:91/255.0 alpha:1.0];
+    
+    self.backgroundColor                = backgroundColor;
+    self.layer.cornerRadius             = CGRectGetWidth(self.bounds) / 2;
+    self.layer.borderWidth              = 1 / [UIScreen mainScreen].scale;
+    self.layer.borderColor              = borderColor.CGColor;
+    self.layer.allowsEdgeAntialiasing   = YES;
+    
+    CAShapeLayer *leftArrowShape    = [CAShapeLayer layer];
+    leftArrowShape.lineWidth        = 3 / [UIScreen mainScreen].scale;
+    leftArrowShape.masksToBounds    = NO;
+    leftArrowShape.lineCap          = kCALineCapRound;
+    leftArrowShape.strokeColor      = strokeColor.CGColor;
+    leftArrowShape.fillColor        = backgroundColor.CGColor;
+    
+    CGRect fr       = self.bounds;
+    CGFloat width   = CGRectGetWidth(fr);
+    CGFloat height  = CGRectGetHeight(fr);
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, CGRectGetMidX(fr) + (width / 20), CGRectGetMidY(fr) - (height / 6));
+    CGPathAddLineToPoint(path, NULL, CGRectGetMidX(fr) - (width / 8), CGRectGetMidY(fr));
+    CGPathAddLineToPoint(path, NULL, CGRectGetMidX(fr) + (width / 20), CGRectGetMidY(fr) + (height / 6));
+    
+    leftArrowShape.path = path;
+    CGPathRelease(path);
+    
+    [self.layer addSublayer:leftArrowShape];
+}
+
+#pragma mark - Common UI setup
+
+- (void)setupCornerRadius {
+    
+    self.layer.cornerRadius             = CGRectGetWidth(self.bounds) / 2;
+    self.layer.masksToBounds            = NO;
+    self.layer.allowsEdgeAntialiasing   = YES;
+}
+
+- (void)setupShadow {
+    
+    self.layer.shadowColor      = [UIColor blackColor].CGColor;
+    self.layer.shadowOpacity    = 0.15;
+    self.layer.shadowRadius     = 3;
+    self.layer.shadowOffset     = CGSizeMake(0.0f, 3.0f);
+}
+
+- (void)setupTargets {
+    
+    [self addTarget:self action:@selector(normalState)      forControlEvents:UIControlEventTouchUpInside];
+    [self addTarget:self action:@selector(normalState)      forControlEvents:UIControlEventTouchDragOutside];
+    [self addTarget:self action:@selector(highlitedState)   forControlEvents:UIControlEventTouchDown];
+}
+
+#pragma mark - Button State Animation
+
+- (void)normalState {
+    
+    self.transform  = CGAffineTransformMakeScale(1, 1);
+    self.alpha      = 1;
+}
+
+- (void)highlitedState {
+    
+    self.transform  = CGAffineTransformMakeScale(1.1, 1.1);
+    self.alpha      = 0.75;
+}
+
+@end
+
